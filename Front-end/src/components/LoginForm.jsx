915f2c67b2f66f96cccf4373223078e821a338c4
Mohammed Wardah <moewardah77@gmail.com>
2024-03-29 09:18:31 +0300

    Minor loginPage fixes

--- Front-end/src/components/LoginForm.jsx
+++ Front-end/src/components/LoginForm.jsx
@@ -2,36 +2,43 @@ import { Form, Link } from "react-router-dom";
 
 import classes from "./LoginForm.module.css";
 import loginImg from "../assets/loginPage/loginphoto2.png";
-import logo from "../assets/loginPage/uni.png";
+import uniLogo from "../assets/loginPage/uni.png";
+import EtLogo from "/ETlogo.png";
 
 export default function LoginForm() {
   return (
     <>
-      <div className={classes.container}>
-        <div className={classes.loginLeft}>
-          <img src={loginImg} alt="login image" />
-        </div>
-        <Form>
-          <div className={classes.loginRight}>
-            <div className={classes.logo}>
-              <img src={logo} alt="logo" />
-            </div>
-            <div className={classes.formControl}>
-              <h1>Login to your account</h1>
-              <span className="material-symbols-outlined">person</span>
-              <input type="text" name="name" placeholder="Username" />
-              <span className="material-symbols-outlined">lock</span>
-              <input type="password" name="password" placeholder="Password" />
-              <button>Login</button>
-            </div>
-            <div>
-              <Link to="/">Return Home</Link>
-              <Link to="/">Forgot Password ?</Link>
-            </div>
+      <section className={classes.main}>
+        <div className={classes.container}>
+          <div className={classes.loginLeft}>
+            <img src={loginImg} alt="login image" />
           </div>
-        </Form>
-      </div>
-      <h1 className={classes.credits}>Powered by ET Electric Technology</h1>
+          <Form>
+            <div className={classes.loginRight}>
+              <div className={classes.logo}>
+                <img src={uniLogo} alt="uskudar logo" />
+              </div>
+              <div className={classes.formControl}>
+                <h1>Login to your account</h1>
+                <span className="material-symbols-outlined">person</span>
+                <input type="text" name="name" placeholder="Username" />
+                <span className="material-symbols-outlined">lock</span>
+                <input type="password" name="password" placeholder="Password" />
+                <button>Login</button>
+              </div>
+              <div>
+                <Link to="/">Return Home</Link>
+                <Link to="/">Forgot Password ?</Link>
+              </div>
+            </div>
+          </Form>
+        </div>
+        <div className={classes.credits}>
+          <h1>
+            Powered By <img src={EtLogo} alt="ET Logo" /> Electric Technology
+          </h1>
+        </div>
+      </section>
     </>
   );
 }
--- Front-end/src/pages/Login.jsx
+++ Front-end/src/pages/Login.jsx
@@ -1,10 +1,5 @@
 import LoginForm from "../components/LoginForm";
-import classes from "./Login.module.css";
 
 export default function LoginPage() {
-  return (
-    <section className={classes.specialBg}>
-      <LoginForm />
-    </section>
-  );
+  return <LoginForm />;
 }
